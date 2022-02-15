befd59ede3e14cd6bbc2d64ca744ebfa92e06d6c
NavneetBhatti <navneetbhatti13@gmail.com>
2022-02-14 20:53:08 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from "./Components/Navigation/Nav";
+import Footer from './Components/Footer';
 
-function App() {
+
+
+
+ 
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <BrowserRouter>
+      <div>
+        <Nav />
+        {/* <Routes>
+          <Route path='/' element={<Tasks />} /> 
+          <Route path='tasklist/:id' element={<TaskList />} />
+          <Route path='about' element={<About />} />
+          <Route path='posts' element={<Posts />} />
+         <Route path='todo' element={<Todo />} />  
+         <Route path='*' element={<FileNotFound />} />
 
+        </Routes>*/}
+        {/* <Footer />  */}
+      </div>
+    </BrowserRouter>
+  );
+};
 export default App;
+ 
+
+
+
---
+++ src/Components/Navigation/Nav.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { MenuList } from "./Menu";
+import "./Nav.css";
+
+const Navbar = () => {
+  const [click, setclick] = useState(false);
+  const menuList = MenuList.map(({ url, title }, index) => {
+    return (
+      <li key={index}>
+        <NavLink exact to={url} activeClassName="active">
+          {title}
+        </NavLink>
+      </li>
+    );
+  });
+
+  
+
+  return (
+    <nav>
+      <div className="logo">
+        Car<font>Rental</font>
+      </div>
+     
+      <ul className={click ? "menu-list" : "menu-list close"}>{menuList}</ul>
+    </nav>
+  );
+};
+
+export default Navbar;
