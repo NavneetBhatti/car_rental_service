9adbb9a5ec92c7f2ddf8d5b4f37cf1f84f777826
james sebastian <jamessebastian916@gmail.com>
2022-03-18 00:05:00 -0400

    formatted

--- src/App.js
+++ src/App.js
@@ -10,29 +10,26 @@ import Register from './Pages/Register';
 import Login from './Pages/Login';
 
 
-
- 
-
 const App = () => {
-  return (
-    <BrowserRouter>
-      <div>
-        <Nav />
-        <Routes>
-      <Route path="/" element={<Home />} /> 
-      <Route path="Carlist" element={<Carlist />} />
-      <Route path="Payment" element={<Payment />} />
-      <Route path="Booking" element={<Bookings />} />
-      <Route path="Register" element={<Register />} />
-      <Route path="Sign" element={<Login/>}/>
-      </Routes>
-        <Footer /> 
-      </div>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<div>
+				<Nav />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="Carlist" element={<Carlist />} />
+					<Route path="Payment" element={<Payment />} />
+					<Route path="Booking" element={<Bookings />} />
+					<Route path="Register" element={<Register />} />
+					<Route path="Sign" element={<Login />} />
+				</Routes>
+				<Footer />
+			</div>
+		</BrowserRouter>
+	);
 };
 export default App;
- 
+
 
 
 
