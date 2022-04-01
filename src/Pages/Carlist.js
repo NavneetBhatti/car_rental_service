2c2811ceb9ab54cb36ef453611d8d51219b3dd8f
NavneetBhatti <navneetbhatti13@gmail.com>
2022-04-01 19:16:50 -0400

    add car list

--- src/Pages/Carlist.js
+++ src/Pages/Carlist.js
@@ -1,93 +1,65 @@
-import { Footer } from "../Components/Footer";
+import React, { useEffect, useState } from "react";
+import {Link } from "react-router-dom";
+import axios from 'axios';
 
-const Carlist = () => {
-    
-    return (
-        <>
-        
-          
-          <div class="container my-4">
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-              <div class="col">
-                <div class="card shadow-sm"style={{height: "200px",width:"400px"}}>
-                  <img src="https://media.istockphoto.com/photos/red-generic-sedan-car-isolated-on-white-background-3d-illustration-picture-id1189903200?k=20&m=1189903200&s=612x612&w=0&h=L2bus_XVwK5_yXI08X6RaprdFKF1U9YjpN_pVYPgS0o="style={{height: "200px",width:"400px"}}/>
-                  <div class="card-body">
-                  <button >Book Now</button>
 
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group"></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card shadow-sm"style={{height: "200px",width:"400px"}}>
-                  <img src="https://images.unsplash.com/photo-1605559424843-9e4c228bf1c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNhcnN8ZW58MHx8MHx8&w=1000&q=80" style={{height: "200px",width:"400px"}}/>
-                  <div class="card-body">
-                  <button >Book Now</button>
 
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group"></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card shadow-sm" style={{height: "200px",width:"400px"}}>
-                  <img src="https://media.wired.com/photos/5d09594a62bcb0c9752779d9/1:1/w_1500,h_1500,c_limit/Transpo_G70_TA-518126.jpg"style={{height: "200px",width:"400px"}} />
-    
-                  <div class="card-body">
-                  <button >Book Now</button>
 
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group"></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-    
-              <div class="col">
-                <div class="card shadow-sm" style={{height: "200px",width:"400px",marginTop: "50px"}}>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUZHjGoW2dF9iKRj5DDV8csbQsstNt2gc_Uw&usqp=CAU" style={{height: "200px",width:"400px"}}/>
-                  <div class="card-body">
-                  <button >Book Now</button>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group"></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card shadow-sm" style={{height: "200px",width:"400px",marginTop: "50px"}}>
-                  <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2022-chevrolet-corvette-z06-1607016574.jpg?crop=0.737xw:0.738xh;0.181xw,0.218xh&resize=640:*"style={{height: "200px",width:"400px"}} />
-    
-                  <div class="card-body">
-                  <button >Book Now</button>
-
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group"></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card shadow-sm"style={{height: "200px",width:"400px",marginTop: "50px"}}>
-                  <img src="https://www.cnet.com/a/img/CSTqzAl5wJ57HHyASLD-a0vS2O0=/940x528/2021/04/05/9e065d90-51f2-46c5-bd3a-416fd4983c1a/elantra-1080p.jpg" style={{height: "200px",width:"400px"}} />
-    
-                  <div class="card-body">
-                    
-                    <button >Book Now</button>
-
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group"></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* <Footer/> */}
-        </>
+const CarList=()=> {
+  const [users, setUsers] = useState([]);
+  const getUsers = async () => {
+      const response = await axios.get(
+        'http://localhost:5000/api/cars/'
       );
+      setUsers(response.data);
+   };
+
+
+  useEffect(()=>{
+    getUsers();
+  },[])
+
+
+  return (
+   <>
+      <div className="container-fluid mt-5">
+        <div className="row text-center">
+
+          {
+            users.map((currElem)=>{
+              return(
+                <div className="col-10 col-md-4 mt-5 mb-5">
+                <div class="card p-2  ">
+                  <div class="d-flex align-items-center">
+                    <div class="image"><img src={currElem.image} class="rounded" width="155" height="100" /> </div>
+                    <div class="ml-3 w-100">
+                      <h4 class="mb-0 mt-0 textLeft">{currElem.name}</h4> <span className="textLeft">{currElem.type}</span>
+                      <div class="price">
+
+                        <span className="textRight">{currElem.price}</span>
+                        <Link to="/Booking"><button class="btn2">
+                           Book Now
+                         </button>
+                        </Link>
+                        
+                      </div>
+    
+                    </div>
+    
+                  </div>
+    
+                </div>
+    
+              </div>
+              )
+            })
+          }
+    
+      </div>
+
+      </div>
+   </>
+  );
 }
-export default Carlist
+
+export default CarList;
