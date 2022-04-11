8bf51085d348b60b7f63485455cea077a3018f09
NavneetBhatti <navneetbhatti13@gmail.com>
2022-04-11 16:50:33 -0400

    car list

--- src/Pages/Carlist.js
+++ src/Pages/Carlist.js
@@ -1,71 +1,78 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import "../Components/css/carlist.css";
+import {Link } from "react-router-dom";
+import axios from 'axios';
 
-const CarList = () => {
+
+
+
+const CarList=()=> {
   const [users, setUsers] = useState([]);
   const getUsers = async () => {
-    let token = localStorage.getItem("token");
-    let config = {
-      header: {
-        "Content-Type": "application/json",
-        //'x-auth-token': token,
-      },
-    };
-    const response = await axios.get("http://localhost:5000/api/cars/", config);
-    setUsers(response.data);
-    console.log(response.data);
-    console.log("--test");
-  };
+        let token = localStorage.getItem('token');
+        let config={
+          header:{
+          'Content-Type' : 'application/json',
+          'x-auth-token': token,
+        },
+      };
+      const response = await axios.get(
+          'http://localhost:5000/api/cars/',
+          config
+      );
+      setUsers(response.data);
+      console.log(response.data);
+      console.log("--test");
 
-  useEffect(() => {
+   };
+
+
+  useEffect(()=>{
     getUsers();
-  }, []);
+  },[])
+
 
   return (
-    <>
+   <>
       <div className="container-fluid mt-5">
-        <div className="container">
-          {users.map((currElem) => {
-            let imgurl = "http://localhost:5000/uploads/" + currElem.image;
-            return (
-              <div className="card p-2  ">
-                <div className="d-flex align-items-center" id="wrapper">
-                  <div className="image">
-                    <img
-                      src={imgurl}
-                      className="rounded"
-                      width="155"
-                      height="100"
-                    />{" "}
-                  </div>
-                  <div className="ml-3 w-100" id="left" >
-                    <h4 className="mb-2 mt-2 textLeft">{currElem.name}</h4>{" "}
-                    <span className="textLeft">{currElem.type}</span>
-                    <div className="price">
-                      <span className="textRight">
-                        $ {currElem.price} / Day
-                      </span>
-                      <div id="right">
-                        <Link to={`/Booking/${currElem._id}`}>
-                          <button className="btn2">
-                            {/* <Link to={'/Booking/'+currElem._id+'/'+currElem.name+'/'+currElem.type+'/'+currElem.price+'/'+currElem.image}><button className="btn2"> */}
-                            Book Now
-                          </button>
+        <div className="row text-center">
+
+          {
+            users.map((currElem)=>{
+              return(
+                <div className="col-10 col-md-4 mt-5 mb-5">
+                <div class="card p-2  ">
+                  <div class="d-flex align-items-center">
+                    <div class="image"><img src={currElem.image} class="rounded" width="155" height="100" /> </div>
+                    <div class="ml-3 w-100">
+                      <h4 class="mb-0 mt-0 textLeft">{currElem.name}</h4> <span className="textLeft">{currElem.type}</span>
+                      <div class="price">
+
+                        <span className="textRight">{currElem.price}/Day</span>
+                        {/* both ways are correct */}
+                        <Link to={`/Booking/${currElem._id}`}><button class="btn2">  
+                        {/* <Link to={'/Booking/'+currElem._id+'/'+currElem.name+'/'+currElem.type+'/'+currElem.price+'/'+currElem.image}><button class="btn2"> */}
+
+                              Book Now
+                         </button>
                         </Link>
                       </div>
-                      {/* both ways are correct */}
+    
                     </div>
+    
                   </div>
+    
                 </div>
+    
               </div>
-            );
-          })}
-        </div>
+              )
+            })
+          }
+    
       </div>
-    </>
+
+      </div>
+   </>
   );
-};
+}
 
 export default CarList;
